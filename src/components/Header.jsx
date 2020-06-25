a2ba21a628ec88d9df12736b0d6373a6d04025cb
Tom Moliterno <rustbukt307@Toms-MacBook-Air.local>
2020-06-25 01:55:38 -0400

    styling improved, some elements restructured for styling ease

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 class Header extends Component {
   render() {
     return (
       <header className="header-main">
-        <h1>
-          <Link to='/'>Tom Moliterno</Link>
-        </h1>
-        <h2>
-          Software Developer
-        </h2>
-    </header>
-    );
+        <div className='name'>
+          <h1><Link to='/'>Tom Moliterno</Link></h1>
+          <h3 className='title'>Software Developer</h3>
+        </div>
+        
+        <div className='links'>
+          <a target='_blank' href='https://resume.creddle.io/resume/me9n8un0bg'>Resume</a>
+          <a target='_blank' href='https://github.com/rustbukt307'>GitHub</a>
+          <a target='_blank' href='https://www.linkedin.com/in/tommoliterno/'>LinkedIn</a>
+          </div>
+      </header>
+    )
   }
 }
 
-export default Header;
+export default Header
